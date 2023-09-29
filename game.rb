db6f0ccf79db0156ac69da923cff81851e240e97
Bob Foxx <bobfox141@gmail.com>
2023-09-29 14:20:40 -0500

    Update game.rb

--- game.rb
+++ game.rb
@@ -1,2 +1,25 @@
 #!/usr/bin/env ruby
 # file: game.rb
+class game
+  def introduction()
+    print("This is the Game of Sink the Bismarck.")
+    print("A game of naval combat, both you and the computer have shells, some number greater than 20, in your")
+    print("magazines. ")
+    print("Both you and the enemy have high explosive shells.")
+    print("Your mission is to sink the enemy vessel before it")
+    print("sinks you.")
+    print("The number of shells availalbe ")
+    return
+  end
+
+  def takingonwater()
+    print("Your vessel is taking on water.")
+    return
+  end
+  def davy_jones()
+    print("The enemy is headed for Davy Jones Locker!")
+    return
+  end
+
+
+end
